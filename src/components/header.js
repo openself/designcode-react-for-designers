0b78c93d05ab26fe55c7aa02d80c2613e98a5873
openself <openself@bk.ru>
2019-03-03 22:06:36 +0300

    Урок 3

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,23 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "./header.css"
+import logo from "../images/logo-designcode.svg"
 
 const Header = ({ siteTitle }) => (
-  <header>
+  <header className="Header">
+    <div className="HeaderGroup">
+      <Link to="/">
+        <img src={logo}/>
+      </Link>
+      <Link to="/courses">Courses</Link>
+      <Link to="/downloads">Downloads</Link>
+      <Link to="/workshops">Workshops</Link>
+      <Link to="/sign-in">Sign In</Link>
+      <Link to="/try">
+        <button>Try for free</button>
+      </Link>
+    </div>
   </header>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+
 
 const IndexPage = () => (
   <Layout>
@@ -19,9 +19,7 @@ const IndexPage = () => (
 
     </div>
 
-    {/*<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>*/}
-    {/*<Image/>*/}
-    {/*</div>*/}
+
 
   </Layout>
 )

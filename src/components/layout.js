b50bf78c96aa5c296a2d37557447919b6d4aad1e
openself <openself@bk.ru>
2019-03-03 17:57:55 +0300

    Урок 2

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,6 +12,15 @@ import { StaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import "./layout.css"
 
+import WebFont from 'webfontloader'
+
+WebFont.load({
+  sustom: {
+    families: ['SF Pro Text', 'SF Pro Display'],
+    urls: ['../fonts/fonts.css']
+  }
+})
+
 const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
@@ -25,20 +34,11 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <Header/>
+        <div>
           <main>{children}</main>
           <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+            © {new Date().getFullYear()}
           </footer>
         </div>
       </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,26 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
-import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Главная" keywords={[`design`, `application`, `react`]} />
-    <h1>Привет, друг</h1>
-    <p>Добро пожаловать на наш сайт.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to<br/>
+          design and code<br/>
+          <span>modern apps</span>
+        </h1>
+        <p>Complete courses about the best tools and design systems.
+          Prototype and build apps with React and Swift.</p>
+        <Link to="/page-2/">Watch the video </Link>
+      </div>
+
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+
+    {/*<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>*/}
+    {/*<Image/>*/}
+    {/*</div>*/}
+
   </Layout>
 )
 
